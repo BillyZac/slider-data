e161a7f976a0a1b545a9e742117963d52046e058
Zachary Smith <billyzacsmith@gmail.com>
2015-11-11 16:20:22 -0700

    Add the D3 library. The slider controls the width of a div.

--- master.js
+++ master.js
@@ -1,15 +1,21 @@
-// var $value = $('.slider').val()
+// Initialize slider
+$('.slider').val(0)
 
-$('.sliderOutput').text('50')
+
+$('.sliderOutput').text($('.slider').val())
 
 // When the slider is moved, update the displayed
 $('.slider').on('input', function() {
   var $sliderValue = $(this).val()
   $('.sliderOutput').text($sliderValue)
+  d3.selectAll(".bloop")
+    .style("width", $sliderValue*4 + "px")
 })
 
 // Same thing with vanilla js
+/*
 var sliders = document.getElementsByClassName('slider')
 sliders[0].addEventListener('input', function() {
   console.log('focused', sliders[0].value)
 })
+*/
